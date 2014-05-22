f96271f61796e3b50647026a5536ebff04002f6a
Tom Houpt <tom@houptlab.org>
2014-05-22 19:48:10 +0000

    updated project settings; found missing files

---
+++ BCMeanValue.h
@@ -0,0 +1,51 @@
+//
+//  BCMeanValue.h
+//  TongueTwister
+//
+//  Created by Tom Houpt on 10/4/29.
+//  Copyright 2010 Behavioral Cybernetics. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface BCMeanValue : NSObject {
+
+		unsigned long num;
+		double mean;
+		double stdev;
+		double SEM;
+
+}
+
+-(double)mean;
+-(unsigned long)num;
+-(double)stdev; // NOTE: we divide sum of squares by (num - 1) to get sample standard deviation
+-(double)SEM;  // NOTE: we divide stdev by sqrt (num) to get SEM
+
+
+-(void) zeroMean;
+-(void) sumMean:(double) x;
+		// add x to the current sum (stored in mean)
+		// if x is "missing value" then it is not added
+		// increments num by 1
+
+-(void) sumNonZeroMean:(double) x;
+		// sometimes we require the mean to be composed of only non-zero values
+		// so x is added to mean if x != 0
+
+-(void) divideMean;
+		// divide the mean (the summed up x's) by the num to get the mean
+
+// stdev and standard error of the mean is found the same way, by adding up squared deviations
+-(void) sumErr:(double) x;
+-(void) sumNonZeroErr:(double) x;
+-(void) divideErr; 
+// assuming all the error terms have been added, now divide by (n-1) for stdev and sqrt(n) for SEM
+
+-(NSString *) justMean2Text;
+-(NSString *) justSEM2Text;
+-(NSString *) mean2Text;
+-(NSString *) mean2FileText;
+
+@end
---
+++ BCMeanValue.m
@@ -0,0 +1,91 @@
+//
+//  BCMeanValue.m
+//  TongueTwister
+//
+//  Created by Tom Houpt on 10/4/29.
+//  Copyright 2010 Behavioral Cybernetics. All rights reserved.
+//
+
+#import "BCMeanValue.h"
+
+#define SQUARE(x) ((x)*(x))
+
+@implementation BCMeanValue
+
+-(double)mean {return (mean); }
+-(unsigned long)num {return (num); }
+-(double)stdev; {return (stdev); }
+-(double)SEM {return (SEM); }
+
+-(void) zeroMean { mean = 0; stdev = 0; SEM = 0; num = 0; }
+
+-(void) sumMean:(double) x { mean+= x; num++;}
+
+-(void) sumNonZeroMean:(double) x { if (x != 0) {mean+= x; num++;}}
+
+-(void) divideMean { if (num > 0) mean /= num; }
+
+-(void) sumErr:(double) x { stdev += SQUARE( (x - mean) ); }
+
+-(void) sumNonZeroErr:(double) x { if (x != 0) stdev += SQUARE( (x - mean) ); }
+
+-(void) divideErr {
+	// NOTE: we divide both stdev by (num - 1) for sample standard deviation
+	// NOTE: we divide stdev by sqrt(num) to get SEM
+	if (num > 1) {
+		stdev /= (num - 1);
+		stdev = sqrt(stdev); // final stdev
+		SEM = stdev/ sqrt((double)(num));
+	}
+	else {
+		stdev = 0;
+		SEM = 0;
+	}
+}
+
+-(NSString *) justMean2Text {
+	
+	NSString *text;	
+	
+	if (num == 0) text = [NSString stringWithString:@"--0--"];
+	else text = [NSString stringWithFormat:@"%.2Lf", mean];
+	
+	return text;
+	
+	
+}
+
+-(NSString *) justSEM2Text {
+	
+	NSString *text;	
+
+	if (num < 2)  text = [NSString stringWithString:@"--0--"];
+	else [NSString stringWithFormat:@"%.2Lf", SEM ];
+	
+	return text;
+}
+
+-(NSString *) mean2Text {
+	
+	NSString *text;	
+	
+	if (num == 0)  text = [NSString stringWithString:@"--0--"];
+	else if (num == 1) text = [NSString stringWithFormat:@"%.2Lf (%lu)", mean,num];
+	else text = [NSString stringWithFormat:@"%.2Lf±%.2Lf(%lu)", mean,SEM,num];
+	
+	return text;
+}
+
+-(NSString *) mean2FileText {
+	
+	NSString *text;	
+	
+	if (num == 0) [NSString stringWithString:@"\t\t\t"];
+	else if (num == 1) text = [NSString stringWithFormat:@"%Lf\t\t%lu\t", mean,num];
+	else text = [NSString stringWithFormat:@"%Lf\t%Lf\t%lu\t", mean,SEM,num];
+				 
+	return text;				 
+}
+
+
+@end
