64db0cdc2906eca2c10690bca81c160150006071
Tom Houpt <tom@houptlab.org>
2014-06-13 20:51:52 +0000

    converted enums to NS_ENUM, NS_OPTIONS

--- BCFontUtilities.h
+++ BCFontUtilities.h
@@ -9,13 +9,24 @@
 #import <Foundation/Foundation.h>
 
 // formatting bit flags
-#define kBCBoldFlag 1
-#define kBCItalicFlag 2
-#define kBCUnderlinedFlag 4
 
-enum justify { LEFT_JUSTIFIED = 0, CENTER_JUSTIFIED, RIGHT_JUSTIFIED };
+typedef NS_OPTIONS(NSUInteger,BCFontFormatOptions) {
+    kBCPlainFlag = 0,
+    kBCBoldFlag = 1 << 0,
+    kBCItalicFlag = 1 << 1,
+    kBCUnderlinedFlag = 1 << 2
+};
 
-enum verticalAlignment { TOP_ALIGNED = 0, MIDDLE_ALIGNED, BOTTOM_ALIGNED };
+typedef NS_ENUM(NSInteger, BCFontHorzAlignmentOptions)  { LEFT_JUSTIFIED = 0,
+    CENTER_JUSTIFIED,
+    RIGHT_JUSTIFIED
+};
+
+typedef NS_ENUM(NSInteger, BCFontVertAlignmentOptions) {
+    TOP_ALIGNED = 0,
+    MIDDLE_ALIGNED,
+    BOTTOM_ALIGNED
+};
 
 void SetFontStyleControlSelection(NSSegmentedControl *fontStyleControl, NSInteger styleFlags);
 NSInteger  GetFontStyleFlagsFromControlSelection(NSSegmentedControl *fontStyleControl);
